3225195c7bdebefc7a9e8099ded362d253468f98
sahbi96 <sahbinour@gmail.com>
2024-01-22 10:58:13 +0100

    first commit

---
+++ contact/mail.js
@@ -0,0 +1,31 @@
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAcrka5gtnmm79jPRAeeH5pAHlt03vW2OI",
+//   authDomain: "risper-contact.firebaseapp.com",
+//   databaseURL: "https://risper-contact-default-rtdb.europe-west1.firebasedatabase.app",
+//   projectId: "risper-contact",
+//   storageBucket: "risper-contact.appspot.com",
+//   messagingSenderId: "22912564791",
+//   appId: "1:22912564791:web:7df899eb785fd19d8309b2"
+// };
+
+// // initialize
+//  app = initializeApp(firebaseConfig);
+// // reference
+
+
+// var risperContact = firebase.database().ref('risperContact')
+
+// document.getElementById('risperContact').addEventListener('submit',submitForm);
+
+// function submitForm(e){
+//   e.preventDefault();
+//   var name = getElementVal('name');
+//   var emailid = getElementVal('emailid');
+//   var msgContent = getElementVal('msgContent');
+
+//   console.log(mail, emailid, msgContent);
+// }
+
+// const getElementVal = (id)=>{
+//   return document.getElementById(id).value;
+// }
